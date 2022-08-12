21a436779eab5284fa4af25a43a352ad23747e11
mxb2ll <111109120+mxb2ll@users.noreply.github.com>
2022-08-12 14:43:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ def get_count():
   return delta.days
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  next = datetime.strptime(birthday + "-" + str(date.today().year), "%Y-%m-%d")
+'''  
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
+'''
   return (next - today).days
 
 def get_words():
